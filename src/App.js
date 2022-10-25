0ec9d63321175c4aa7701315145e769784b187b7
Edwin Vasco <edwin.vasco@gmail.com>
2022-10-25 13:37:47 -0500

    creacion de directorios components y styles

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          DEVE SOLUTION MINTIC 2022
         </a>
       </header>
     </div>
